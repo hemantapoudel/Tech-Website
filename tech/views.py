9edaafa1bfa07a3c5af0049d6ddfae557887af51
hemantapoudel <hemantapoudel14@gmail.com>
2020-09-15 21:17:08 +0545

    creating views

--- tech/views.py
+++ tech/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.views.generic.base import View
+from .models import *
 
 # Create your views here.
-class HomeView(View):
+class BaseView(View):
+    frontend_view={}
+
+class HomeView(BaseView):
     def get(self,request):
-        return render(request,'index.html')
+        self.frontend_view['posts']=Posts.objects.all()
+        return render(request,'index.html',self.frontend_view)
 
 class SingleView(View):
     def get(self,request):
